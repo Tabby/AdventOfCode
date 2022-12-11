20e6bf103f036785256ee9ee22c93c007871646e
Tabby Cromarty <tabby@brightmoon.online>
2022-12-11 20:03:06 +0000

    Day 1, 2022

--- 2022/1.rb
+++ 2022/1.rb
@@ -2,16 +2,36 @@
 
 module AOC
   class D1
-    # param input [?] input array
+    attr_accessor :elves
+
+    # param input [Array<integer?>] input array
     def initialize(input)
       @input = input
+      @elves = [0]
+      count_calories
     end
 
     def p1
+      elves.last
     end
 
     def p2
+      elves.slice(-3, 3).sum
     end
 
+    private
+
+    def count_calories
+      current_elf = 0
+      @input.each do |cals|
+        if cals == ''
+          current_elf += 1
+          elves << 0
+        else
+          elves[current_elf] += cals.to_i
+        end
+      end
+      elves.sort!
+    end
   end
 end
--- 2022/spec/1_spec.rb
+++ 2022/spec/1_spec.rb
@@ -5,26 +5,30 @@ require_relative '../1'
 describe AOC::D1 do
   describe 'Day 1' do
     context 'Example input' do
+      subject do
+        AOC::D1.new(File.read('spec/input/1_example.txt').split("\n"))
+      end
+
       it 'Puzzle 1 gives correct answer' do
-        subject = AOC::D1.new([199, 200, 208, 210, 200, 207, 240, 269, 260, 263])
-        expect(subject.p1).to eq(7)
+        expect(subject.p1).to eq(24_000)
       end
 
       it 'Puzzle 2 gives correct answer' do
-        subject = AOC::D1.new([607, 618, 618, 617, 647, 716, 769, 792])
-        expect(subject.p2).to eq(5)
+        expect(subject.p2).to eq(45_000)
       end
     end
 
     context 'Real input' do
-      subject { AOC::D1.new(File.read('spec/input/1.txt').split.map(&:to_i)) }
+      subject do
+        AOC::D1.new(File.read('spec/input/1.txt').split("\n"))
+      end
 
       it 'Puzzle 1 gives correct answer' do
-        expect(subject.p1).to eq(1451)
+        expect(subject.p1).to eq(65_912)
       end
 
       it 'Puzzle 2 gives correct answer' do
-        expect(subject.p2).to eq(1395)
+        expect(subject.p2).to eq(195_625)
       end
     end
   end
